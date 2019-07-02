c808298cbdd0725fc34378017d1e64d4844a6aa5
吕彪 <1396799517@qq.com>
2019-07-02 22:43:16 +0800

    提交内容： 1. 添加Semaphore使用样例

--- src/main/java/com/cn/thread/CyclicBarrierExample.java
+++ src/main/java/com/cn/thread/CyclicBarrierExample.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  * CyclicBarrier让一组线程到达一个屏障（也可以叫同步点）时被阻塞，
  * 直到最后一个线程到达屏障时，屏障才会开门，所有被屏障拦截的线程才会继续运行。
  */
-public class CyclicBarrierTest {
+public class CyclicBarrierExample {
 
     static Integer cbNum = 5;
 
---
+++ src/main/java/com/cn/thread/SemaphoreExample.java
@@ -0,0 +1,46 @@
+package com.cn.thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @autor: lvbiao
+ * @version: 1.0
+ * @descript:
+ * @date: 2019-07-02 22:23
+ * Semaphore（信号量）是用来控制同时访问特定资源的线程数量，它通过协调各个线程，以保证合理的使用公共资源。
+ */
+public class SemaphoreExample {
+    /**
+     * 共设置两个许可
+     */
+    static Semaphore semaphore = new Semaphore(2);
+
+    public static void main(String[] args){
+        for (int i = 0; i < 6; i++) {
+            new Thread(new Work(i)).start();
+        }
+    }
+
+    static class Work implements Runnable{
+        Integer num = 0;
+        Work(Integer num){
+            this.num = num;
+        }
+        @Override
+        public void run() {
+            try {
+                // 获取许可， 这里同时一共只会有两个线程拥有许可，其他线程会阻塞
+                semaphore.acquire();
+                TimeUnit.MILLISECONDS.sleep((long) (Math.random()*1000));
+                System.out.println(num + "开始工作,剩余排队数" + semaphore.getQueueLength());
+                TimeUnit.SECONDS.sleep(1L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                semaphore.release();
+            }
+        }
+    }
+
+}
