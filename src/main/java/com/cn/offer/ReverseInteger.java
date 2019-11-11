fc3035843795e155c3e6976b41d642393b3f7500
吕彪 <1396799517@qq.com>
2019-11-11 18:50:04 +0800

    提交内容： 1. 反转整数

---
+++ src/main/java/com/cn/offer/ReverseInteger.java
@@ -0,0 +1,43 @@
+package com.cn.offer;
+
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * <p>
+ * 示例 1:
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ * <p>
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: lvbiao
+ * @date: 2019-11-11 17:15
+ */
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
